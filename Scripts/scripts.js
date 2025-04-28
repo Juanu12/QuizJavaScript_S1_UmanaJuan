204ea7040e8a9b064d73abbf50877284b3eb5519
Juanu12 <juanferumana@hotmail.com>
2025-04-28 08:42:51 -0500

    feat: :zap: Improving my scripts and structure

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -3,24 +3,30 @@ async function llamarapi(){
     let data = await response.json()
    return data
 }
-
 async function searchingdata(){
 let response = await fetch (` https://api.weatherapi.com/v1/current.json?key=69ab410e39134867a8d114611252804&q=Floridablanca `)
 let info = await response.json()
 let name = document.getElementById( "Name")
 name.src = info["location"][0]
-Name.innerHTML = `${info["location"]}`;
-}
+Name.innerHTML = `<p> ${info["location"]["name"]},${info["location"]["country"]} </p>`;
 
+}
 
 
 async function showingdta(){
-data = await llamarapi()
-let name = document.getElementById( "Name")
-info = await response.json()
-name.src = info["location"][0]
-Name.innerHTML = `${info["location"]}`;
-searchingdata(data)
+data2 = await llamarapi() 
+searchingdata(data2)
 }
+
+async function clouddata(){
+     let response = await fetch (` https://api.weatherapi.com/v1/current.json?key=69ab410e39134867a8d114611252804&q=Floridablanca `)
+let info = await response.json()
+let name = document.getElementById( "Name")
+name.src = info["location"][0]
+Name.innerHTML = `<p> ${info["location"]["name"]},${info["location"]["country"]} </p>`;
+
+
+}
+
 
 showingdta()
